b2bfd30bf9ce62496badba2523c36a6b111a50fe
Allan <allan.kowiti@student.moringaschool.com>
2022-08-04 03:52:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const { validateUseBuiltInsOption } = require("@babel/preset-env/lib/normalize-options");
+
 // Code your solutions in this file
+writeCards(["Guadalupe","Ollie","Aki","surprise"]);
+
+function writeCards(names, event ) {
+  const newNames = []
+  for(let i = 0 ; i < names.length; i++){
+  newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newNames;
+}
+ 
+ countDown(10)
+function countDown(number){
+  while (number >= 0){
+  console.log(number);
+  number --;
+  }
+}
